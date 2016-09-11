9a76484754ada43b974f4811cbb6b284a29912fe
jamjam <andy@thewindop.com>
2016-09-11 22:30:27 +0100

    made a bt more generic

--- mmr/MmrTest/simulation/vivado/run_tb.tcl
+++ mmr/MmrTest/simulation/vivado/run_tb.tcl
@@ -1,27 +1,19 @@
 
-#------------------------------------------------------------------------------
-# Variables for Vivado tool flow, sim, synth and implememtation.
-#------------------------------------------------------------------------------
+set PROJ_NAME testSimFlow
+set PROJ_PATH ./
 
-set PROJ_NAME project_5_sim_flow
-set PROJ_PATH C:/Users/a/arty
-set PROJ_PART xc7a35ticsg324-1L
-set PROJ_BRD  digilentinc.com:arty:part0:1.1
+lappend design_files src/pc_ctrl.v
+lappend design_files src/pc_ctrl_pm0.v
+lappend design_files src/pc_ctrl_pm1.v
+lappend design_files src/pc_ctrl_axi4_reg_if.v
 
-lappend design_files C:/Users/a/Dropbox/verilogBanks/v/example.v
-lappend design_files C:/Users/a/Dropbox/verilogBanks/v/example_b1.v
-lappend design_files C:/Users/a/Dropbox/verilogBanks/v/example_b0.v
-lappend design_files C:/Users/a/Dropbox/verilogBanks/v/example_axi4_reg_if.v
+lappend sim_files src/tb_top.v
 
-lappend sim_files C:/Users/a/Dropbox/verilogBanks/tb_top.v
+create_project ${PROJ_NAME} ${PROJ_PATH}/${PROJ_NAME} -part xc7a35ticsg324-1L -force
 
-#------------------------------------------------------------------------------
-# Runscript. Generally you should not need to modify commands.
-#------------------------------------------------------------------------------
-create_project ${PROJ_NAME} ${PROJ_PATH}/${PROJ_NAME} -part ${PROJ_PART} -force
+set_property board_part digilentinc.com:arty:part0:1.1 [current_project]
 
-set_property board_part ${PROJ_BRD} [current_project]
-
+# add_files -norecurse {C:/Users/a/Dropbox/verilogBanks/v/example.v C:/Users/a/Dropbox/verilogBanks/v/example_b1.v C:/Users/a/Dropbox/verilogBanks/v/example_b0.v C:/Users/a/Dropbox/verilogBanks/v/example_axi4_reg_if.v}
 add_files -norecurse ${design_files}
 
 update_compile_order -fileset sources_1
@@ -31,13 +23,14 @@ set_property SOURCE_SET sources_1 [get_filesets sim_1]
 add_files -fileset sim_1 -norecurse ${sim_files}
 
 update_compile_order -fileset sim_1
+update_compile_order -fileset sim_1
 
 launch_simulation
 
 run all
 
-add_files -fileset constrs_1 C:/Users/a/Dropbox/verilogBanks/cons.xdc
-set_property target_constrs_file C:/Users/a/Dropbox/verilogBanks/cons.xdc [current_fileset -constrset]
+add_files -fileset constrs_1     src/cons.xdc
+set_property target_constrs_file src/cons.xdc [current_fileset -constrset]
 
 launch_runs synth_1 -jobs 2 -verbose
 wait_on_run synth_1
